2a7e05d3b1f6fc3afae01bdea02cdc6be994cdb9
1197777 <1197777@student.roc-nijmegen.nl>
2022-06-01 14:36:33 +0200

    Opzet project

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+include_once('connection.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Document</title>
+</head>
+<body>  
+    <div class="bannerimage">
+        <nav>
+            <img class= "logo1"src="img/logo.png" alt="image1">
+            <div class="logoh1">
+                <h1>TravelSpace</h1>
+            </div>
+            <ul>
+                <li><a href="index.php" class="active">Home</a></li>
+                <li><a href="informatie.php">Informatie</a></li>
+                <li><a href="overons.php">Over ons</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+        </nav>
+    </div> 
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Document</title>
+</head>
+<body>   
+     <nav>
+         <img class= "logo1"src="img/logo.png" alt="image1">
+          <div class="logoh1">
+             <h1>TravelSpace</h1>
+         </div>
+          <ul>
+              <li><a href="index.php" class="active">Home</a></li>
+              <li><a href="informatie.php">Informatie</a></li>
+              <li><a href="overons.php">Over ons</a></li>
+              <li><a href="contact.php">Contact</a></li>
+              <li><a href="login.php">Login</a></li>
+         </ul>
+     </nav>
+</body>
+</html>
