aeaea1a2caf70fa565a40177770545a44cb60a43
1197777 <1197777@student.roc-nijmegen.nl>
2022-06-08 14:23:40 +0200

    youri

--- contact.php
+++ contact.php
@@ -11,6 +11,28 @@ include_once('connection.php');
     <title>Document</title>
 </head>
 <body>
+    <div class="contact">
+
     
+<nav class="nav-area">
+            <img class= "logo1"src="img/logo.png" alt="image1">
+            <div class="logoh1">
+                <h1>TravelSpace</h1>
+            </div>
+            <ul>
+                <li><a href="index.php" class="active">Home</a></li>
+                <li><a href="bestemmingen.php">Bestemmingen</a> 
+                <ul>             
+				<li><a href="#">Europa</a></li>	
+				<li><a href="#">Midden-Oosten</a></li>
+				<li><a href="#">Noord-Amerika</a>	</li>
+				<li><a href="#">Azië</a></li>
+                </ul></li>
+                <li><a href="overons.php">Over ons</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+        </nav>
+    </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -12,19 +12,46 @@ include_once('connection.php');
 </head>
 <body>  
     <div class="bannerimage">
-        <nav>
+        <nav class="nav-area">
             <img class= "logo1"src="img/logo.png" alt="image1">
             <div class="logoh1">
                 <h1>TravelSpace</h1>
             </div>
             <ul>
                 <li><a href="index.php" class="active">Home</a></li>
-                <li><a href="informatie.php">Informatie</a></li>
+                <li><a href="bestemmingen.php">Bestemmingen</a> 
+                <ul>             
+				<li><a href="#">Europa</a></li>	
+				<li><a href="#">Midden-Oosten</a></li>
+				<li><a href="#">Noord-Amerika</a>	</li>
+				<li><a href="#">Azië</a></li>
+                </ul></li>
                 <li><a href="overons.php">Over ons</a></li>
                 <li><a href="contact.php">Contact</a></li>
                 <li><a href="login.php">Login</a></li>
             </ul>
         </nav>
+        <div class="middentekst">
+            <h1>Verre Reizen. Voor Iedereen Uniek.</h1>
+            <h2>Individuele reizen op maat.</h2>
+        </div>
+        <div class="container-box">
+             <label for="bestemming"></label>
+                <select id="country" name="country">
+                    <option value="">Spanje</option>
+                    <option value="#">Zuid-Afrika</option>
+                </select>
+                <label for="soortreis"></label>
+                <select id="country" name="country">
+                    <option value="#">Wintersport</option>
+                    <option value="#">Zon</option>
+                </select>
+                <label for="datum"></label>
+                <select id="country" name="country">
+                    <option value="januari">Januari</option>
+                    <option value="februari">Februari</option>
+                </select>
+        </div>
     </div> 
 </body>
 </html>
